2eddecc951fda3a3db2662d859a9a67151494e62
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-12-11 16:02:08 -0500

    Add intro.v and rearrange chapters

---
+++ B_Unification/intro.v
@@ -0,0 +1,56 @@
+
+(** * Introduction *)
+
+
+
+
+(** * Unification *)
+
+
+(** ** Syntatic Unification *)
+
+
+
+(** ** Semantic Unification *)
+
+
+
+(** ** Boolean Unification *)
+
+
+
+
+
+(** * Formal Verification *)
+
+
+(** ** Proof Assistance *)
+
+
+
+(** ** Verifying Systems *)
+
+
+
+(** ** Verifying Theories *)
+
+
+
+
+
+(** * Importance *)
+
+
+
+
+
+(** * Development *)
+
+
+(** ** Data Structures *)
+
+
+(** ** Algorithms *)
+
+
+
