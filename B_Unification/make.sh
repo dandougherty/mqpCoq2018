22286c2c83b4533c8b798007eea09a6b85428587
Dylan Richardson <dylanrichardson1996@gmail.com>
2019-04-22 12:40:33 -0400

    Add title page to report

--- B_Unification/make.sh
+++ B_Unification/make.sh
@@ -3,9 +3,15 @@ set -e
 
 make
 
+preamble="\\usepackage{graphicx} \\graphicspath{ {.} } \
+\\newtheorem{definition}{Definition}[section]"
+
 coqdoc -toc -interpolate -utf8 --latex -R . B_Unification -o all.tex intro.v \
 	terms.v lowenheim_formula.v lowenheim_proof.v list_util.v poly.v \
-	poly_unif.v sve.v -p "\\newtheorem{definition}{Definition}[section]"
+	poly_unif.v sve.v -p "$preamble"
+
+awk 'NR==20{print "\\input{title.tex}"}1' all.tex > \
+	all.tmp.tex && mv all.tmp.tex all.tex
 
 pdflatex all
 
