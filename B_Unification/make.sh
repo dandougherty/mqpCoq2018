6c5844c3c83397787db16729e9b1e9032183efd4
Dylan Richardson <dylanrichardson1996@gmail.com>
2019-04-24 15:26:35 -0400

    Add the abstract

--- B_Unification/make.sh
+++ B_Unification/make.sh
@@ -11,7 +11,7 @@ coqdoc -toc -interpolate -utf8 --latex -R . B_Unification -o all.tex intro.v \
 	terms.v lowenheim_formula.v lowenheim_proof.v list_util.v poly.v \
 	poly_unif.v sve.v -p "$preamble"
 
-awk 'NR==20{print "\\input{title.tex}"}1' all.tex > \
+awk 'NR==20{print "\\input{front_matter.tex}"}1' all.tex > \
 	all.tmp.tex && mv all.tmp.tex all.tex
 
 pdflatex all
